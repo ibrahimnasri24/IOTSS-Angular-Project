e3332687a82df3638862f86616cb35fd1a102a02
Ibraim Nasri <ibrahimnasri24@gmail.com>
2022-11-20 13:48:05 +0200

    made color picker dynamic

--- src/app/Note/color-picker/color-picker.component.ts
+++ src/app/Note/color-picker/color-picker.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Colors } from 'src/app/model/colors';
 
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.css']
+  styleUrls: ['./color-picker.component.css'],
 })
 export class ColorPickerComponent implements OnInit {
+  colors = Colors.getColors();
+  maxW: string = '0px';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  openColorPicker = () => {
+    this.maxW = '120px';
+  };
 
+  closeColorPicker = () => {
+    this.maxW = '0px';
+  };
+
+  ngOnInit(): void {}
 }
